a4cc4fae42eb2ee0282512eb7dbb4a06694b7875
Roguelied <110971527+Roguelied@users.noreply.github.com>
2023-05-05 23:32:29 +0300

    Delete strange include, add list of available commands to send

--- UserInterface.h
+++ UserInterface.h
@@ -1,8 +1,6 @@
 #ifndef INFO_SYSTEM_CPP_USERINTERFACE_H
 #define INFO_SYSTEM_CPP_USERINTERFACE_H
 
-#include <utility>
-
 #include "Utility.h"
 
 
@@ -15,6 +13,7 @@ private:
     static string AdminPsswrd;
     static string ServerPsswrd;
     static string ServerLoggin;
+
 public:
     static vector<Button> Buttons;
     static void DrawFrame(int x1,int y1,int x2,int y2);
@@ -68,6 +67,7 @@ public:
     void Action(){
         Container2();
     }
+
     Button(int x1, int y1, string Text, function<string(int, int, const char*, char)> Function);
     Button(int x1, int y1, string Text, function<void(int, int)> Function);
     Button(int x1, int y1, string Text, function<void()> Function);
--- main.cpp
+++ main.cpp
@@ -3,22 +3,19 @@
 #include "UserInterface.h"
 
 
-void  printKek1(){
-    cout << "Hello\n";
-}
-
-string jopa(int x) {
-    cout << "123";
-    return "123";
-}
-
+//SERver commands UREG UFND UDEL
+//                DADD DFND DDEL
+//                RES DALL UALL
 int main() {
     //InitializeSettings();
+
     Client::Initialize();
-    Client::AskServer("DEL 22");
-    Client::AskServer("DEL 44");
-    Client::AskServer("FND LoginTest%PassTest%");
-    Client::AskServer("REG NewLogin%NewPassw%");
+    //Client::AskServer("UDEL 22");
+    //Client::AskServer("DDEL 2");
+    //Client::AskServer("UREG ABOBA%SKRILLEX%");
+    Client::AskServer("DADD DCLASS%08.05.2024/18:23%12%NONE%");
+
+    //Client::AskServer("REG NewLogin%NewPassw%");
     Client::CloseConnection();
     wait;
 }
